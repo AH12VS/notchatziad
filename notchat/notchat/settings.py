13635380d67328b2077874db06f5d080feafe2f0
Ali Hosseini <ali1234wew2x3.new@gmail.com>
2024-05-21 13:31:15 +0330

    fresh project commit

---
+++ notchat/notchat/settings.py
@@ -0,0 +1,229 @@
+# ========== IMPORTS ========== #
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# ========== ENDIMPORTS ========== #
+
+
+# ========== ENV ========== #
+load_dotenv()
+# ========== ENDENV ========== #
+
+
+# ========== BASEDIR ========== #
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+# ========== ENDBASEDIR ========== #
+
+
+# ========== SECURE ========== #
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# ========== ENDSECURE ========== #
+
+
+# ========== DEBUG ========== #
+DEBUG = os.environ.get("DEBUG") == "True"
+# ========== ENDDEBUG ========== #
+
+
+# ========== HOSTS ========== #
+ALLOWED_HOSTS = ["*"]
+# ========== ENDHOSTS ========== #
+
+
+# ========== APPS ========== #
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+APPS = [
+    "users.apps.UsersConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + APPS
+# ========== ENDAPPS ========== #
+
+
+# ========== MIDDLEWARE ========== #
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+# ========== ENDMIDDLEWARE ========== #
+
+
+# ========== URLCONF ========== #
+ROOT_URLCONF = "notchat.urls"
+# ========== ENDURLCONF ========== #
+
+
+# ========== TEMPLATES ========== #
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+# ========== ENDTEMPLATES ========== #
+
+
+# ========== WSGI ========== #
+WSGI_APPLICATION = "notchat.wsgi.application"
+# ========== ENDWSGI ========== #
+
+
+# ========== DB ========== #
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+# ========== ENDDB ========== #
+
+
+# ========== AUTH ========== #
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+# ========== ENDAUTH ========== #
+
+
+# ========== INTERNATIONALIZATION ========== #
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+# ========== ENDINTERNATIONALIZATION ========== #
+
+
+# ========== USER ========== #
+AUTH_USER_MODEL = "users.UserModel"
+# ========== ENDUSER ========== #
+
+
+# ========== STATIC ========== #
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# ========== ENDSTATIC ========== #
+
+
+# ========== MEDIA ========== #
+MEDIA_ROOT = [BASE_DIR / "media/"]
+MEDIA_URL = "/media/"
+# ========== ENDMEDIA ========== #
+
+
+# ========== PK ========== #
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# ========== ENDPK ========== #
+
+
+# ========== LOGGING ========== #
+LOGGING = {
+    "version": 1,
+    "loggers": {
+        "django": {
+            # "handlers": ["file", "console", "debughandler"],
+            "handlers": ["file", "console"],
+            "level": "DEBUG",
+        }
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO",  # level set to INFO to just save useful logs
+            "class": "logging.FileHandler",
+            "filename": "./logs/logs.log",
+            "formatter": "fileformatter",
+        },
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "consoleformatter",
+        },
+        # "debughandler": {
+        #     "level": "DEBUG",  # level set to DEBUG to save all logs
+        #     "class": "logging.FileHandler",
+        #     "filename": f"{USER_WORK_SPACE}/logs/sch/debug.log",
+        #     # "filename": "./logs/debug.log",
+        #     "formatter": "fileformatter",
+        # },
+    },
+    "formatters": {
+        "fileformatter": {
+            # "format": "*** \n[{asctime}] ||| {levelname} ||| {name} ||| {module} ||| {message} \n***",
+            "format": "<<<[{asctime}]  [{levelname}]  [{module}]  [{message}]>>>",
+            "style": "{",
+        },
+        "consoleformatter": {
+            "format": "***\n[{asctime}]  [{levelname}]  [{message}]\n***",
+            "style": "{",
+        },
+    },
+}
+# ========== ENDLOGGING ========== #
+
+
+# ========== COOKIE ========== #
+SESSION_COOKIE_AGE = 864000
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# ========== ENDCOOKIE ========== #
+
+
+# ========== HSTS ========== #
+# SECURE_HSTS_SECONDS = 31536000  # 1 year
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# ========== ENDHSTS ========== #
+
+
+# ========== SECUREREDIRECT ========== #
+# SECURE_SSL_REDIRECT = True
+# ========== ENDSECUREREDIRECT ========== #
+
+
+# ========== XSSPROTECTION ========== #
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# ========== ENDXSSPROTECTION ========== #
---
+++ notchat/users/models.py
@@ -0,0 +1,121 @@
+from django.db import models
+
+# use to make an custom User Model
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import identify_hasher
+
+
+class UserModelManager(BaseUserManager):
+    def create_user(
+        self,
+        password,
+        email="",
+        passwd="",
+    ):
+        # check email if its empty or not
+        if not email:
+            raise ValueError("Email is Required")
+        #
+        #
+        user = self.model(
+            email=self.normalize_email(email),
+            passwd=passwd,
+        )
+        # user.set_password(password)
+        # user.passwd = password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        password,
+        email="",
+        passwd="",
+    ):
+        user = self.create_user(
+            email,
+            password,
+            passwd=f"{passwd}{password}",
+        )
+        user.is_active = True
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class UserModel(AbstractBaseUser):
+    email = models.EmailField(verbose_name="ایمیل", max_length=250, unique=True)
+    username = models.CharField(verbose_name="نام کاربری", max_length=250)
+    img_prof = models.ImageField(
+        verbose_name="عکس",
+        upload_to="img/users/",
+        blank=True,
+        null=True,
+    )
+
+    # default django user fields
+    date_joined = models.DateTimeField(
+        verbose_name="زمان پیوستن",
+        auto_now_add=True,
+        unique=True,
+    )
+    last_login = models.DateTimeField(verbose_name="آخرین ورود", auto_now=True)
+    updated = models.DateTimeField(verbose_name="بروزرسانی", auto_now=True)
+    #
+    is_admin = models.BooleanField(verbose_name="مدیر", default=False)
+    is_active = models.BooleanField(verbose_name="فعال", default=False)
+    is_staff = models.BooleanField(verbose_name="کارمند", default=False)
+    is_superuser = models.BooleanField(verbose_name="سوپریوزر", default=False)
+    is_cms_user = models.BooleanField(verbose_name="کاربر سی ام اس", default=False)
+    # end default django user fields
+
+    # check for signup
+    is_in_signup = models.BooleanField(verbose_name="در مرحله ی ثبت", default=True)
+    passwd = models.CharField(verbose_name="رمز", max_length=200, null=True, blank=True)
+    # end check for signup
+
+    # token
+    tmp_token = models.CharField(
+        verbose_name="توکن موقت",
+        max_length=10,
+        null=True,
+        blank=True,
+    )
+    tmp_token_time = models.DateTimeField(
+        verbose_name="زمان توکن موقت",
+        null=True,
+        blank=True,
+    )
+    # end token
+
+    # object manger
+    objects = UserModelManager()
+    # object manger
+
+    USERNAME_FIELD = "email"  # use email for username
+
+    # show first part of email (-> before @)
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        try:
+            hasher = identify_hasher(self.password)
+        except:
+            self.set_password(self.passwd)
+            self.passwd = ""
+
+        super(UserModel, self).save(*args, **kwargs)
+
+    # default django user method
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    # end default django user method
